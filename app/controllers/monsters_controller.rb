b38db95aecfa4f30b19e707ddd1de4c597fba5fd
J. Cohen <hostelmarketing@gmail.com>
2018-04-30 12:04:10 -0700

    Allow saving the new :active state

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -2,4 +2,16 @@ class MonstersController < ApplicationController
   def index
     render json: Monster.all
   end
+
+  def update
+    @monster = Monster.find(params[:id])
+    @monster.update_attributes(monster_params)
+    render json: @monster
+  end
+
+  private
+
+  def monster_params
+    params. require(:monster).permit(:active)
+  end
 end
